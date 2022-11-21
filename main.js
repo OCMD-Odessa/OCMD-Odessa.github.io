26cd91201d8cc7fd4aab58b1a962ead797248327
OCMD-Odessa <116883365+OCMD-Odessa@users.noreply.github.com>
2022-11-21 22:29:11 +0100

    removed transition

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-function closeTransition() {
-    document.getElementById("transition_block").style.width = "100%";
-}
-function openTransition() {
-    document.getElementById("transition_block").style.width = "0%";
-}
-
 function openPage(next_page) {
-    closeTransition()
-    setTimeout(function() {location.href = next_page;}, 1000)
-}
-
-function main() {
-    openTransition();
+    location.href = next_page;
 }
